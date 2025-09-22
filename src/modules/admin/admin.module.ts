6d89e60de896576b1a7328b31495cd51010fc9d3
BlaiseMkouta <blaiselele221@gmail.com>
2025-09-22 10:28:41 +0100

    update admin.module.ts to include admin.controller & admin.service

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,13 +1,13 @@
 import { Module } from "@nestjs/common";
 import { WithdrawalQueueController } from "./withdrawalQueue.controller";
 import { WithdrawalProcessorService } from "../../services/scheduler/withdrawalProcessor.service";
+import { AdminController } from "./admin.controller";
+import { AdminService } from "./admin.service";
+import { EmailService } from "@/services/email.service";
 
 @Module({
-  controllers: [WithdrawalQueueController],
-  providers: [WithdrawalProcessorService],
+  controllers: [WithdrawalQueueController, AdminController],
+  providers: [WithdrawalProcessorService, AdminService, EmailService],
   exports: [WithdrawalProcessorService],
 })
 export class AdminModule {}
-
-
-
